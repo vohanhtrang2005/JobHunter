9a426f96ceafefa4ba2f8dc4b9fd4059cfd0b5d6
vohanhtrang2005 <vohanhtrang2019@gmail.com>
2025-10-10 09:39:11 +0700

    exception

---
+++ src/main/java/com/job/domain/RestResponse.java
@@ -0,0 +1,41 @@
+package com.job.domain;
+
+public class RestResponse<T> {
+    private int statusCode;
+    private String error;
+
+    private Object message;
+    private T data;
+    public RestResponse(int statusCode, String error, Object message, T data) {
+        this.statusCode = statusCode;
+        this.error = error;
+        this.message = message;
+        this.data = data;
+    }
+    public int getStatusCode() {
+        return statusCode;
+    }
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+    public String getError() {
+        return error;
+    }
+    public void setError(String error) {
+        this.error = error;
+    }
+    public Object getMessage() {
+        return message;
+    }
+    public void setMessage(Object message) {
+        this.message = message;
+    }
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+    
+    
+}
