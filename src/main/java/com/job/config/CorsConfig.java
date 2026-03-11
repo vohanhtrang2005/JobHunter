33a57014913aa1789ee80221a357993e1be7fc7e
vohanhtrang2005 <vohanhtrang2019@gmail.com>
2026-03-11 10:49:14 +0700

    commit

--- src/main/java/com/job/config/CorsConfig.java
+++ src/main/java/com/job/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@configuration
+@Configuration
 public class CorsConfig {
 @Bean
     public CorsConfigurationSource corsConfigurationSource() {
